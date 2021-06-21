4ad2ef5278b140c2664342e5cbbf447c4191fd9d
Cr7st <819032438@qq.com>
2021-06-21 13:58:43 +0800

    update Help() in main.cpp

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ void Init()
     cout << space8 << "    \"quit;\": command to quit the system" << endl;
     cout << space8 << "       \"help;\": command to get help" << endl;
     cout << "      \"file;\": command to read SQL query from a file" << endl;
-    cout << "\n" << space8 << "  Bowen Zheng, Yunqi Chen and Zehao Zhu" << endl;
+    cout << "\n" << " Copyright(c) 2021 Bowen Zheng, Yunqi Chen and Zehao Zhu" << endl;
     cout << longline << endl;                                                                                             
 }
 
@@ -67,11 +67,53 @@ string ConsoleGetCommand()
     string res;
     getline(cin, tmp);
     while (tmp[tmp.length() - 1] != ';'){
-        cout << "          ";
+        cout << "         ";
         res = res + " ";
         res = res + tmp;
         getline(cin, tmp);
     }
     res = res + " " + tmp;
+    res.erase(0, 1);
     return res;
+}
+
+void Help()
+{
+    cout << R"(+------------------------------------------------------------------------------------------------+)" << std::endl;
+	cout << R"(|A simple example to create a student databae named STU                                          |)" << std::endl;
+	cout << R"(+------------------------------------------------------------------------------------------------+)" << std::endl;
+	cout << R"(|Create database  : create database STU;                                                         |)" << std::endl;
+	cout << R"(|Use database     : use database STU;                                                            |)" << std::endl;
+	cout << R"(|Show database    : show databases;                                                              |)" << std::endl;
+	cout << R"(|Create Table     : create table student(id int primary, socre double, name char(20));           |)" << std::endl;
+	cout << R"(|Insert Record(1) : insert into student(id,score,name)values(1,95.5,ZhangSan);                   |)" << std::endl;
+	cout << R"(|Insert Record(2) : insert into student(id,name)values(2,LiSi); Note:LiSi has no score           |)" << std::endl;
+	cout << R"(|Delete Table     : delete from student where id = 1; Note: ZhangSan is deleted                  |)" << std::endl;
+	cout << R"(|Select Table(1)  : select * from student where id = 2;                                          |)" << std::endl;
+	cout << R"(|Select Table(2)  : select * from student where id > 1 and score < 98;                           |)" << std::endl;
+	cout << R"(|Drop database    : drop database STU;                                                           |)" << std::endl;
+	cout << R"(|Quit             : quit;                                                                        |)" << std::endl;
+	cout << R"(+------------------------------------------------------------------------------------------------+)" << std::endl;
+	cout << R"(|Note             : Anytime you want to end MiniSQL use "quit;" command please.                  |)" << std::endl;
+	cout << R"(+------------------------------------------------------------------------------------------------+)" << std::endl;
+}
+
+vector<string> FileGetCommand(string file_name)
+{
+    vector<string> command_set;
+    ifstream in_file;
+    in_file.open(file_name);
+    while (!in_file.eof()){
+        string tmp;
+        string res;
+        getline(in_file, tmp);
+        while (tmp[tmp.length() - 1] != ';'){
+            res = res + " " + tmp;
+            getline(in_file, tmp);
+        }
+        res = res + " " + tmp;
+        res.erase(0, 1);
+        command_set.push_back(res);
+    }
+    return command_set;
 }
